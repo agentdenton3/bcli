df92fb24d1a4ac0bf1a2b4121b51025eadf30ea8
agent_denton <andybrychenko@gmail.com>
2020-12-29 23:19:25 +0200

    trying to connect

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,38 @@ static PORT_SETTINGS: SerialPortSettings = SerialPortSettings {
 };
 
 // TODO: maybe add new(), and check on creation if data is valid
+#[derive(Debug)]
 pub struct SerialData {
-    l: u8,
+    opcode: u8,
     size: u8,
-    data: u16,
+    data: Option<u16>,
+}
+
+impl SerialData {
+    pub fn new(opcode: u8, size: u8, data: Option<u16>) -> Self {
+        Self { opcode, size, data }
+    }
 }
 
 /// parse raw data from serial port
 /// n - length with \n
 /// l - length without \n
-pub fn parse_serial(raw: &str) -> SerialData {
+pub fn parse_serial(raw: &str) -> Option<SerialData> {
     // get bytes from serial
-    // first element is size of msg in bytes
     let s = raw.split("\r\n").collect::<Vec<_>>();
-    let size = match s.get(0) {
+    // first element is opcode
+    let opcode = match s.get(0) {
+        Some(elem) => elem.parse::<u8>().unwrap_or(56),
+        None => 0,
+    };
+    // second element is size of msg in bytes
+    let size = match s.get(1) {
         Some(elem) => elem.parse::<u8>().unwrap_or(0),
         None => 0,
     };
     // get length of the msg and then compare it with size
     // l + 1 because we are considering \n at the end of msg
-    let (data, l) = match s.get(1) {
+    let (data, l) = match s.get(2) {
         Some(elem) => {
             let l = elem.len() as u8;
             let d = elem.parse::<u16>().unwrap_or(0);
@@ -41,7 +53,11 @@ pub fn parse_serial(raw: &str) -> SerialData {
         }
         None => (0, 1),
     };
-    SerialData { l, size, data }
+    if size == l {
+        Some(SerialData::new(opcode, size, Some(data)))
+    } else {
+        None
+    }
 }
 
 pub fn test_serial(fd: &str) {
@@ -53,17 +69,20 @@ pub fn test_serial(fd: &str) {
                 Ok(_b) => {
                     let raw =
                         std::str::from_utf8(&buff).unwrap_or("bad string");
-                    let sd = parse_serial(&raw);
-                    if sd.size == sd.l {
-                        println!("raw = {:?}", raw);
-                        println!("size = {}", sd.size);
-                        println!("data = {}", sd.data);
-                        println!("--------------------");
+                    println!("{:?}", raw);
+                    if let Some(sd) = parse_serial(&raw) {
+                        println!("st = {:?}", sd);
                     }
-                    // TODO: find a better way
-                    // clear buffer manually to align data,
-                    // because buff.clear() does not workk
-                    buff.iter_mut().for_each(|x| *x = 0);
+                    // if sd.size == sd.l {
+                    //     println!("raw = {:?}", raw);
+                    //     println!("size = {}", sd.size);
+                    //     println!("data = {}", sd.data);
+                    //     println!("--------------------");
+                    // }
+                    // // TODO: find a better way
+                    // // clear buffer manually to align data,
+                    // // because buff.clear() does not workk
+                    // buff.iter_mut().for_each(|x| *x = 0);
                 }
                 Err(_e) => {}
             }
